00e176804a0ed70fb9ed550edd2e985170e9f7f1
DevionGames <73476107+DevionGames@users.noreply.github.com>
2020-12-21 22:33:19 +0100

    SetCameraRelative option in SetPosition action

--- Assets/Devion Games/Triggers/Scripts/Runtime/Sequence/Actions/Transform/SetPosition.cs
+++ Assets/Devion Games/Triggers/Scripts/Runtime/Sequence/Actions/Transform/SetPosition.cs
@@ -11,18 +11,26 @@ namespace DevionGames
         private TargetType m_Target = TargetType.Player;
         [SerializeField]
         private Vector3 m_Position=Vector3.zero;
+        [SerializeField]
+        private bool m_SetCameraRelative = true;
 
         private Transform m_Transform;
-
+        private Transform m_CameraTransform;
 
         public override void OnStart()
         {
             this.m_Transform = GetTarget(this.m_Target).transform;
+            this.m_CameraTransform = Camera.main.transform;
         }
 
         public override ActionStatus OnUpdate()
         {
+            Vector3 dir =  this.m_CameraTransform.position - this.m_Transform.position;
+
             this.m_Transform.position = this.m_Position;
+            if(this.m_SetCameraRelative)
+                this.m_CameraTransform.position = this.m_Position + dir;
+
             return ActionStatus.Success;
         }
     }
