1567e1f0d0b020a526b8440e5ccf7441259283de
DevionGames <73476107+DevionGames@users.noreply.github.com>
2021-02-01 13:48:18 +0100

    Respawn Effect

---
+++ Assets/Devion Games/Triggers/Scripts/Runtime/Sequence/Actions/GameObject/CompareTag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DevionGames
+{
+    [UnityEngine.Scripting.APIUpdating.MovedFromAttribute(true, null, "Assembly-CSharp")]
+    [Icon(typeof(GameObject))]
+    [ComponentMenu("GameObject/Compare Tag")]
+    public class CompareTag : Action, ICondition
+    {
+        [SerializeField]
+        private TargetType m_Target = TargetType.Self;
+        [SerializeField]
+        private string m_Tag = "Player";
+
+        public override ActionStatus OnUpdate()
+        {
+            GameObject target = GetTarget(this.m_Target);
+            return target.CompareTag(this.m_Tag) ? ActionStatus.Success : ActionStatus.Failure;
+        }
+    }
+}
---
+++ Assets/Devion Games/Triggers/Scripts/Runtime/Sequence/Actions/Transform/SetPositionToTarget.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace DevionGames
+{
+    [UnityEngine.Scripting.APIUpdating.MovedFromAttribute(true, null, "Assembly-CSharp")]
+    [Icon(typeof(Transform))]
+    [ComponentMenu("Transform/Set Position To Target")]
+    public class SetPositionToTarget : Action
+    {
+        [SerializeField]
+        private TargetType m_Target = TargetType.Player;
+        [SerializeField]
+        private string m_Tag = "Respawn";
+        [SerializeField]
+        private Vector3 m_DefaultPosition= Vector3.zero;
+        [SerializeField]
+        private bool m_SetCameraRelative = true;
+
+        private Transform m_Transform;
+        private Transform m_CameraTransform;
+
+        public override void OnStart()
+        {
+            this.m_Transform = GetTarget(this.m_Target).transform;
+            this.m_CameraTransform = Camera.main.transform;
+        }
+
+        public override ActionStatus OnUpdate()
+        {
+            Vector3 dir =  this.m_CameraTransform.position - this.m_Transform.position;
+
+            GameObject[] respawn = GameObject.FindGameObjectsWithTag(this.m_Tag);
+            Transform closestRespawn = GetClosest(this.m_Transform, respawn.Select(x => x.transform).ToArray());
+            Vector3 position = this.m_DefaultPosition;
+            if (closestRespawn != null)
+            {
+                position = closestRespawn.position;
+            }
+
+            this.m_Transform.position = position;
+            if(this.m_SetCameraRelative)
+                this.m_CameraTransform.position = position + dir;
+
+            return ActionStatus.Success;
+        }
+
+        Transform GetClosest(Transform self, Transform[] transforms)
+        {
+            Transform bestTarget = null;
+            float closestDistanceSqr = Mathf.Infinity;
+            Vector3 currentPosition = self.position;
+            foreach (Transform potentialTarget in transforms)
+            {
+                Vector3 directionToTarget = potentialTarget.position - currentPosition;
+                float dSqrToTarget = directionToTarget.sqrMagnitude;
+                if (dSqrToTarget < closestDistanceSqr)
+                {
+                    closestDistanceSqr = dSqrToTarget;
+                    bestTarget = potentialTarget;
+                }
+            }
+
+            return bestTarget;
+        }
+    }
+}
