9d74a8a9074f7a8609231d7f8b4a69cd4c3a6fa1
DevionGames <73476107+DevionGames@users.noreply.github.com>
2021-02-01 12:59:41 +0100

    Execution order
    Start should be executed in StatsHandler before any other start

---
+++ Assets/Devion Games/Stat System/Scripts/Editor/ExecutionOrderValidator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DevionGames.StatSystem
+{
+    [InitializeOnLoad]
+    public class ExecutionOrderValidator
+    {
+        static ExecutionOrderValidator()
+        {
+            var temp = new GameObject();
+
+            var handler = temp.AddComponent<StatsHandler>();
+            MonoScript handlerScript = MonoScript.FromMonoBehaviour(handler);
+            if (MonoImporter.GetExecutionOrder(handlerScript) != -50)
+            {
+                MonoImporter.SetExecutionOrder(handlerScript, -50);
+                Debug.Log("Fixing exec order for " + handlerScript.name);
+            }
+            Object.DestroyImmediate(temp);
+        }
+    }
+}
