5965be32b6c15e620c587769ad120bd4fea8e09d
adrianre12 <adrianrevill@hotmail.com>
2021-08-03 09:53:51 +0100

    Downgrade interface from 8.0 to 7.3 spec

--- Assets/Devion Games/Utilities/Scripts/Runtime/Interfaces/ISaveProvider.cs
+++ Assets/Devion Games/Utilities/Scripts/Runtime/Interfaces/ISaveProvider.cs
@@ -1,31 +1,31 @@
 
 public interface ISaveProvider
 {
-    public void DeleteKey(string key);
+    void DeleteKey(string key);
     /// <summary>
     /// Called after the last load operation.
     /// </summary>
-    public void EndLoad();
+    void EndLoad();
     /// <summary>
     /// Called after the last save operation.
     /// </summary>
-    public void EndSave();
-    public float GetFloat(string key);
-    public float GetFloat(string key, float defaultValue);
-    public int GetInt(string key);
-    public int GetInt(string key, int defaultValue);
-    public string GetString(string key);
-    public string GetString(string key, string defaultValue);
-    public bool HasKey(string key);
-    public void SetFloat(string key, float value);
-    public void SetInt(string key, int value);
-    public void SetString(string key, string value);
+    void EndSave();
+    float GetFloat(string key);
+    float GetFloat(string key, float defaultValue);
+    int GetInt(string key);
+    int GetInt(string key, int defaultValue);
+    string GetString(string key);
+    string GetString(string key, string defaultValue);
+    bool HasKey(string key);
+    void SetFloat(string key, float value);
+    void SetInt(string key, int value);
+    void SetString(string key, string value);
     /// <summary>
     /// Called before the first load operation.
     /// </summary>
-    public void StartLoad();
+    void StartLoad();
     /// <summary>
     /// Called before the first save operation.
     /// </summary>
-    public void StartSave();
+    void StartSave();
 }
